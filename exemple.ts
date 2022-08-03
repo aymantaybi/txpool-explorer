8cef2a9207878a7e0960fe56fd95c3277c94d303
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2022-08-03 06:28:18 +0100

    fix: use hexToNumber for nonce and gas

--- exemple.ts
+++ exemple.ts
@@ -7,14 +7,16 @@ const { WEBSOCKET_PROVIDER } = process.env;
 
 const explorer = new TxpoolExplorer({ host: WEBSOCKET_PROVIDER! });
 
+let pool: "pending" | "queued" = "pending";
+
 let filter = (transaction: Transaction) =>
   transaction.to == "0x7D0556D55ca1a92708681e2e231733EBd922597D";
 
-explorer.watch({ pool: "pending", filter }, (transactions) => {
+explorer.watch({ pool, filter }, (transactions: Transaction[]) => {
   console.log(transactions);
 });
 
 explorer.getPoolContent().then((content) => {
   let { pending, queued } = content;
-  console.log(content);
+  //console.log(content);
 });
--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -4,9 +4,9 @@ import Transaction from "../interfaces/transaction";
 function formatTransaction(transaction: Transaction): Transaction {
   return {
     ...transaction,
-    gas: utils.hexToNumberString(transaction.gas),
+    gas: utils.hexToNumber(transaction.gas),
     gasPrice: utils.hexToNumberString(transaction.gasPrice),
-    nonce: utils.hexToNumberString(transaction.nonce),
+    nonce: utils.hexToNumber(transaction.nonce),
     value: utils.hexToNumberString(transaction.value),
     type: utils.hexToNumberString(transaction.type),
     from: utils.toChecksumAddress(transaction.from),
